e06439fe1027e619ce51450bc524fa24f3556c5e
lalit1 <lalitmalviya085@gmail.com>
2024-03-21 14:27:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Text>search</Text>
+      </View>
+      <View style={styles.list}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10 : 0,
+  },
+  search: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "orange",
+    padding: 16,
+  },
+});
